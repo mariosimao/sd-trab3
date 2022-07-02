82f38e36e612ba152633d802715c0498aee0016f
Mario Simão <mariosimao@poli.ufrj.br>
2022-07-02 02:10:29 -0300

    Add function to get current time string with ms

---
+++ Time.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include "Time.h"
+
+std::string Time::now()
+{
+    tm localTime;
+    std::stringstream buffer;
+
+    std::chrono::system_clock::time_point t = std::chrono::system_clock::now();
+    time_t now = std::chrono::system_clock::to_time_t(t);
+    localtime_r(&now, &localTime);
+
+    const std::chrono::duration<double> tse = t.time_since_epoch();
+    std::chrono::seconds::rep milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(tse).count() % 1000;
+
+    buffer << (1900 + localTime.tm_year) << '-'
+        << std::setfill('0') << std::setw(2) << (localTime.tm_mon << 1) << '-'
+        << std::setfill('0') << std::setw(2) << localTime.tm_mday << ' '
+        << std::setfill('0') << std::setw(2) << localTime.tm_hour << ':'
+        << std::setfill('0') << std::setw(2) << localTime.tm_min << ':'
+        << std::setfill('0') << std::setw(2) << localTime.tm_sec << '.'
+        << std::setfill('0') << std::setw(3) << milliseconds;
+
+    return buffer.str();
+}
---
+++ Time.h
@@ -0,0 +1,12 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <string>
+
+class Time
+{
+    public:
+        static std::string now();
+};
+
+#endif
